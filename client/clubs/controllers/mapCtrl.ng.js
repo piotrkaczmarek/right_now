b9ce90ce5dd131e72e0d15f9ea7d4a1c672b47e6
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2015-06-05 01:09:45 +0200

    Fixes map bug

--- client/clubs/controllers/mapCtrl.ng.js
+++ client/clubs/controllers/mapCtrl.ng.js
@@ -10,41 +10,35 @@
   function MapCtrl(ClubsFactory, $scope, attributeLabels) {
     var vm = this;
     vm.attributeLabels = attributeLabels;
-    vm.warsaw = {
-      lat: 52.233,
-      lng: 21.014,
-      zoom: 14
-    };
-    vm.layers = {
-      baselayers: {
-        osm: {
-            name: 'MapQuest',
-            url: 'http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
-            type: 'xyz'
-        }
-      }
-    };
     vm.clubs = ClubsFactory.clubs();
-    vm.markers = vm.clubs.map(function(club) {
-      return {
-        lat: club.location.lat,
-        lng: club.location.lng,
-        compileMessage: true,
-        getMessageScope: function() {
-          var markerScope = $scope.$new();
-          markerScope.club = club;
-          return markerScope;
-        },
-        message: '<club-card></club-card>'
-        // label: {
-        //   message: $filter('toClubCard')(club),
-        //   // https://github.com/Leaflet/Leaflet.label#options
-        //   options: {
-        //     noHide: true,
-        //     direction: 'auto'
-        //   }
-        // }
-      };
-    });
+    vm.map = {
+      warsaw: {
+        lat: 52.233,
+        lng: 21.014,
+        zoom: 20
+      },
+      layers: {
+        baselayers: {
+          osm: {
+              name: 'MapQuest',
+              url: 'http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+              type: 'xyz'
+          }
+        }
+      },
+      markers: vm.clubs.map(function(club) {
+        return {
+          lat: club.location.lat,
+          lng: club.location.lng,
+          compileMessage: true,
+          getMessageScope: function() {
+            var markerScope = $scope.$new();
+            markerScope.club = club;
+            return markerScope;
+          },
+          message: '<club-card></club-card>'
+        };
+      })
+    };
   }
 })();
